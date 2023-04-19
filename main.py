eb22529fee2f8f96a9b577d4fea5478e28b5f67d
KingTingTheGreat <jting@bu.edu>
2023-04-19 19:19:31 -0400

    use tkinter to select target file and destination directory

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from ascii_artist import AsciiArtist
 from tkinter import Tk
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilename, askdirectory
 
 if __name__ == "__main__":
     artist = AsciiArtist()
     Tk().withdraw()
-    filename = askopenfilename()
-    txt_output = artist.resized(filename)
-    name = filename.split("/")[-1].split(".")[0]
-    print(name)
+    # add label to tk window
+    file_location = askopenfilename(title="Select image file to process")
+    download_location = askdirectory(title="Select destination folder")
+    txt_output = artist.resized(file_location)
+    name = file_location.split("/")[-1].split(".")[0]
     with open(f"text_outputs/{name}.txt", "w") as f:
         for line in txt_output:
             f.write(''.join(line) + "\n")
