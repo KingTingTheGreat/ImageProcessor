c5ae2365ff8d10bf9a4a2726d64e5c84d801cb01
KingTingTheGreat <jting@bu.edu>
2023-04-20 14:32:57 -0400

    uses subprocess to open output file with notepad

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from ascii_artist import AsciiArtist
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename, askdirectory
+from subprocess import Popen
 
 if __name__ == "__main__":
     artist = AsciiArtist()
     Tk().withdraw()
     # add label to tk window
     file_location = askopenfilename(title="Select image file to process")
-    download_location = askdirectory(title="Select destination folder")
+    # download_location = askdirectory(title="Select destination folder")
+    download_location = "text_outputs"
     txt_output = artist.resized(file_location)
     name = file_location.split("/")[-1].split(".")[0]
     with open(f"text_outputs/{name}.txt", "w") as f:
         for line in txt_output:
             f.write(''.join(line) + "\n")
+    Popen(f"notepad.exe text_outputs/{name}.txt")  
