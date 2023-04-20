2ba9aaa19d051647be87c3d8cb147462ab783804
KingTingTheGreat <jting@bu.edu>
2023-04-20 14:38:24 -0400

    added make_all function

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import os
 from ascii_artist import AsciiArtist
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename, askdirectory
 from subprocess import Popen
+
+def make_all():
+    """ Make all the ascii art for all the images in the images folder """
+    artist = AsciiArtist()
+    for filename in os.listdir("images"):
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            txt_output = artist.resized(f"images/{filename}")
+            with open(f"text_outputs/{filename.split('.')[0]}.txt", "w") as f:
+                for line in txt_output:
+                    f.write(''.join(line) + "\n")
 
 if __name__ == "__main__":
     artist = AsciiArtist()
